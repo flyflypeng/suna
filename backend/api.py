bf8a27b9781556e4780240b42137206fe6b929f3
flyflypeng <flyflyflypeng@gmail.com>
2026-01-08 00:53:43 +0800

    support access by node in the same local network
    Signed-off-by: flyflypeng <flyflyflypeng@gmail.com>

--- backend/api.py
+++ backend/api.py
@@ -256,10 +256,10 @@ async def log_requests_middleware(request: Request, call_next):
 allowed_origins = ["https://www.kortix.com", "https://kortix.com"]
 allow_origin_regex = None
 
-# Add staging-specific origins
 if config.ENV_MODE == EnvMode.LOCAL:
     allowed_origins.append("http://localhost:3000")
     allowed_origins.append("http://127.0.0.1:3000")
+    allow_origin_regex = r"^https?://(localhost|127\.0\.0\.1|[0-9\.]+)(:\d+)?$"
 
 # Add staging-specific origins
 if config.ENV_MODE == EnvMode.STAGING:
