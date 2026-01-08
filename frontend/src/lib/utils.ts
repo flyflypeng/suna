7ab9ca27ddea5f40f5f53fc9e3250909f01cd7bb
flyflypeng <flyflyflypeng@gmail.com>
2026-01-08 14:52:54 +0800

    [frontend]: fix crypto.randomUUID func is not defined problem
    Signed-off-by: flyflypeng <flyflyflypeng@gmail.com>

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -75,4 +75,15 @@ export function truncateString(str?: string, maxLength = 50) {
   return str.slice(0, maxLength) + '...';
 }
 
+export function generateRandomId(): string {
+  if (typeof crypto !== 'undefined' && typeof crypto.randomUUID === 'function') {
+    return crypto.randomUUID();
+  }
+
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+    const r = (Math.random() * 16) | 0;
+    const v = c === 'x' ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
 
